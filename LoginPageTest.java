665ccb1cdb92004825e6e7078504ee180713ebe3
unknown <you@example.com>
2025-03-18 00:28:00 +0530

    4 files created

---
+++ LoginPageTest.java
@@ -0,0 +1,47 @@
+package MyMavenAssignment;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import dev.failsafe.internal.util.Assert;
+
+public class LoginPageTest {
+
+  @Test
+    
+
+    public class LoginPageTest2 {
+        private WebDriver driver;
+        private LoginPage loginPage;
+
+        @Before
+        public void setUp() {
+            // Set up WebDriver (make sure to have the ChromeDriver executable in your PATH)
+            System.setProperty("webdriver.chrome.driver", "path/to/chromedriver");
+            driver = new ChromeDriver();
+            driver.get("https://amazon.com/login"); // Replace with your login page URL
+            loginPage = new LoginPage(driver);
+        }
+
+        @Test
+        public void testLoginWithInvalidCredentials() {
+            loginPage.login("invalidUser", "invalidPass");
+            String errorMessage = loginPage.getErrorMessage();
+            org.testng.Assert.assertEquals("Invalid username or password.", errorMessage);
+        }
+
+        @After
+        public void tearDown() {
+            // Close the browser
+            driver.quit();
+        }
+    }
+
+  }
+
